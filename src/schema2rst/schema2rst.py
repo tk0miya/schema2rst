6db6142a7969bd6803d764c6b15954f269f6cfb9
Takeshi KOMIYA <i.tkomiya@gmail.com>
2010-12-17 22:52:54 +0900

    * Import initial version of schema2rst

---
+++ src/schema2rst/schema2rst.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+
+import sqlalchemy
+from sphinx import SphinxDocGenerator
+from pit import Pit
+
+
+class MySQLMetaData:
+    def __init__(self):
+        self.meta = sqlalchemy.MetaData()
+        self.engine = None
+
+    def reflect(self, engine):
+        self.engine = engine
+        self.meta.reflect(engine)
+
+    @property
+    def tables(self):
+        _tables = {}
+        for table_name in self.meta.tables:
+            table = MySQLTable(self.meta.tables[table_name])
+            table.reflect(self.engine)
+            _tables[table_name] = table
+
+        return _tables
+
+
+class MySQLTable:
+    def __init__(self, meta):
+        self.meta = meta
+
+    def reflect(self, engine):
+        self.engine = engine
+
+    @property
+    def name(self):
+        return self.meta.name
+
+    @property
+    def columns(self):
+        for column in self.meta.columns:
+            column = MySQLColumn(column)
+            column.reflect(self.engine)
+            yield column
+
+
+class MySQLColumn:
+    def __init__(self, meta):
+        self.meta = meta
+
+    def reflect(self, engine):
+        self.engine = engine
+
+    @property
+    def fullname(self):
+        return self.meta.name
+
+    @property
+    def name(self):
+        return self.meta.name
+
+    @property
+    def type(self):
+        return self.meta.type
+
+    @property
+    def nullable(self):
+        return self.meta.nullable
+
+    @property
+    def primary_key(self):
+        return self.meta.primary_key
+
+    @property
+    def default(self):
+        return self.meta.default
+
+    @property
+    def doc(self):
+        return self.meta.doc or ""
+
+
+def main():
+    config = Pit.get('gmappers', {'require': {'host': 'localhost',
+                                              'user': 'gmappers',
+                                              'passwd': '',
+                                              'db': 'gmappers'}})
+
+    url = 'mysql://%s:%s@%s/%s' % \
+          (config['user'], config['passwd'], config['host'], config['db'])
+    engine = sqlalchemy.create_engine(url)
+
+    m = MySQLMetaData()
+    m.reflect(engine)
+
+    sphinx = SphinxDocGenerator()
+    sphinx.header(u'Schema: %s' % config['db'])
+
+    for table in m.tables.values():
+        sphinx.header(table.name, '-')
+
+        headers = ['Fullname', 'Name', 'Type', 'NOT NULL', 'PKey', 'Default', 'Comment']
+        sphinx.listtable(headers)
+
+        for c in table.columns:
+            columns = [c.fullname, c.name, c.type, c.nullable, c.primary_key, c.default, c.doc]
+            sphinx.listtable_column(columns)
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/schema2rst/sphinx.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import unicodedata
+
+
+def string_width(string):
+    width = 0
+    for c in string:
+        char_width = unicodedata.east_asian_width(c)
+        if char_width in u"WFA":
+            width += 2
+        else:
+            width += 1
+
+    return width
+
+
+class SphinxDocGenerator:
+    def __init__(self, output=None):
+        self.output = output or sys.stdout
+
+    def out(self, string):
+        self.output.write(str(string) + "\n")
+
+    def header(self, string, char="="):
+        self.out("")
+        self.out(string)
+        self.out(char * string_width(string))
+        self.out("")
+
+    def listtable(self, header=None):
+        self.out(".. list-table::")
+        if header:
+            self.out("   :header-rows: 1")
+
+        self.out("")
+
+        if header:
+            self.listtable_column(header)
+
+    def listtable_column(self, columns):
+        for i, column in enumerate(columns):
+            if i == 0:
+                self.out("   * - %s" % str(column))
+            else:
+                self.out("     - %s" % str(column))
