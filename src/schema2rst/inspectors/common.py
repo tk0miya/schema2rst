53bda80c490b96dbc53bc364f15ed276b286c157
Takeshi KOMIYA <i.tkomiya@gmail.com>
2010-12-19 01:59:58 +0900

    * Fix column.primary_key was incorrect

--- src/schema2rst/inspectors/common.py
+++ src/schema2rst/inspectors/common.py
@@ -13,9 +13,15 @@ class Inspector(reflection.Inspector):
             return string
 
     def get_columns(self, table_name, **kw):
+        primary_keys = self.get_primary_keys(table_name)
         columns = super(Inspector, self).get_columns(table_name, **kw)
         for column in columns:
             column['fullname'] = column['name']
             column['comment'] = ''
+
+            if column['name'] in primary_keys:
+                column['primary_key'] = True
+            else:
+                column['primary_key'] = False
 
         return columns
