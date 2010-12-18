7e3510e4396eb95d982e32635e33f36b796ca538
Takeshi KOMIYA <i.tkomiya@gmail.com>
2010-12-18 18:25:31 +0900

    * Implement MySQLConstraint class

--- src/schema2rst/metadata.py
+++ src/schema2rst/metadata.py
@@ -35,11 +35,23 @@ class MySQLMetaData:
 class MySQLTable:
     def __init__(self, meta):
         self.meta = meta
+        self.keys = []
 
     def reflect(self, engine):
         self.engine = engine
 
         schema_name = os.path.basename(str(self.engine.url))
+
+        query = """SELECT CONSTRAINT_NAME, CONSTRAINT_TYPE
+                   FROM information_schema.table_constraints
+                   WHERE TABLE_SCHEMA = '%s' AND TABLE_NAME = '%s'""" % \
+                   (schema_name, self.name)
+        rs = self.engine.execute(query)
+        for r in rs.fetchall():
+            key = MySQLConstraint(r[0], self.meta, r[1])
+            key.reflect(self.engine)
+            self.keys.append(key)
+
         query = """SELECT TABLE_COMMENT
                    FROM information_schema.Tables
                    WHERE TABLE_SCHEMA = '%s' AND
@@ -63,6 +75,35 @@ class MySQLTable:
             column = MySQLColumn(column)
             column.reflect(self.engine)
             yield column
+
+
+class MySQLConstraint:
+    def __init__(self, name, table, type):
+        self.name = name
+        self.table = table
+        self.type = type
+        self.columns = []
+        self.references = []
+
+    def reflect(self, engine):
+        self.engine = engine
+
+        schema_name = os.path.basename(str(self.engine.url))
+
+        query = """SELECT COLUMN_NAME, REFERENCED_TABLE_NAME,
+                          REFERENCED_COLUMN_NAME
+                   FROM information_schema.table_constraints
+                   LEFT JOIN information_schema.key_column_usage
+                             USING (TABLE_SCHEMA, TABLE_NAME, CONSTRAINT_NAME)
+                   WHERE TABLE_SCHEMA = '%s' AND TABLE_NAME = '%s'""" % \
+                   (schema_name, self.table.name)
+        rs = self.engine.execute(query)
+        meta = self.table.metadata
+        for r in rs.fetchall():
+            self.columns.append(self.table.c[r[0]])
+            if r[1]:
+                reftable = meta.tables[r[1]]
+                self.references.append(reftable.c[r[2]])
 
 
 class MySQLColumn:
