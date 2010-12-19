435d380cfcc6665d86f39439fcb15c71f271af86
Takeshi KOMIYA <i.tkomiya@gmail.com>
2010-12-19 12:18:12 +0900

    * Support extra_comment on mysql

--- src/schema2rst/inspectors/mysql.py
+++ src/schema2rst/inspectors/mysql.py
@@ -24,29 +24,38 @@ class Inspector(common.Inspector):
     def get_columns(self, table_name, **kw):
         columns = super(Inspector, self).get_columns(table_name, **kw)
         for column in columns:
-            query = """SELECT COLLATION_NAME, COLUMN_TYPE,
+            query = """SELECT COLUMN_TYPE, COLLATION_NAME,
                               EXTRA, COLUMN_COMMENT
                        FROM information_schema.Columns
                        WHERE TABLE_SCHEMA = '%s' AND TABLE_NAME = '%s' AND
                              COLUMN_NAME = '%s'""" % \
                        (self.default_schema_name, table_name, column['name'])
             r = self.bind.execute(query).fetchone()
+            column['type'] = self.decode(r[0])
 
-            column['collation_name'] = self.decode(r[0])
-            column['type'] = self.decode(r[1])
-            column['extra'] = self.decode(r[2])
-            column['comment'] = self.decode(r[3])
+            options = []
+            collation_name = self.decode(r[1])
+            if collation_name and collation_name != 'utf8_general_ci':
+                options.append(collation_name)
+
+            extra = self.decode(r[2])
+            if extra:
+                options.append(extra)
 
             comment = self.decode(r[3])
+            extra_comment = ", ".join(options)
             match = re.match('^(.*?)(?:\(|¡Ê)(.*)(?:\)|¡Ë)\s*$', comment)
             if match:
                 column['fullname'] = match.group(1)
                 column['comment'] = match.group(2)
+
+                if extra_comment:
+                    column['comment'] += " (%s)" % extra_comment
             elif comment:
                 column['fullname'] = comment
-                column['comment'] = ''
+                column['comment'] = extra_comment
             else:
                 column['fullname'] = column['name']
-                column['comment'] = ''
+                column['comment'] = extra_comment
 
         return columns
