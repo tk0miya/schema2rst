30796d87879131657ea5418b028a5964f9ff94d8
Takeshi KOMIYA <i.tkomiya@gmail.com>
2013-11-15 23:23:54 +0900

    Update setup.py and make flake8 valid

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-import os, sys
 
 version = '0.1.0'
 long_description = ''
-#long_description = \
-#        open(os.path.join("src","README.txt")).read() + \
-#        open(os.path.join("src","TODO.txt")).read()
 
 classifiers = [
     "Development Status :: 3 - Alpha",
@@ -18,38 +14,41 @@ classifiers = [
 ]
 
 setup(
-     name='schema2rst',
-     version=version,
-     description='schema2rst generates reST file from database schema',
-     long_description=long_description,
-     classifiers=classifiers,
-     keywords=['document','generator'],
-     author='Takeshi Komiya',
-     author_email='i.tkomiya at gmail.com',
-     url='http://tk0miya.bitbucket.org/',
-     license='PSL',
-     packages=find_packages('src'),
-     package_dir={'': 'src'},
-     package_data = {'': ['buildout.cfg']},
-     include_package_data=True,
-     install_requires=[
+    name='schema2rst',
+    version=version,
+    description='schema2rst generates reST file from database schema',
+    long_description=long_description,
+    classifiers=classifiers,
+    keywords=['document', 'generator'],
+    author='Takeshi Komiya',
+    author_email='i.tkomiya at gmail.com',
+    url='https://bitbucket.org/tk0miya/schema2rst',
+    license='PSL',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    package_data={'': ['buildout.cfg']},
+    include_package_data=True,
+    install_requires=[
         'setuptools',
         'sqlalchemy',
         'pyyaml',
-         # -*- Extra requirements: -*-
-     ],
-     extras_require=dict(
-         test=[
-             'Nose',
-             'minimock',
-             'pep8>=1.3',
-         ],
-     ),
-     test_suite='nose.collector',
-     tests_require=['Nose','minimock','pep8'],
-     entry_points="""
-        [console_scripts]
-        schema2rst = schema2rst.schema2rst:main
-        schema2graph = schema2rst.schema2graph:main
-     """,
+    ],
+    extras_require=dict(
+        test=[
+            'Nose',
+            'minimock',
+            'pep8>=1.3',
+        ],
+    ),
+    test_suite='nose.collector',
+    tests_require=[
+        'Nose',
+        'minimock',
+        'pep8>=1.3',
+    ],
+    entry_points="""
+       [console_scripts]
+       schema2rst = schema2rst.schema2rst:main
+       schema2graph = schema2rst.schema2graph:main
+    """,
 )
